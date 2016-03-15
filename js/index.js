0be48ea650cdf4663b2334b13b17bbbc0d85b42f
hzhuangdi <hzhuangdi@corp.netease.com>
2016-03-15 18:46:23 +0800

    debug

---
+++ js/index.js
@@ -0,0 +1,42 @@
+(function(_){
+    var $ = function( selector ){
+      return [].slice.call(document.querySelectorAll(selector))
+    }
+    var container =  $('.m-sld-wrap')[0];
+    var cursors = $('.u-pointer i');
+    cursors.forEach(function(cursor, index){
+      cursor.addEventListener('click', function(){
+        slider.nav(index);
+      })
+    })
+
+    var slider = new Slider({
+      //视口容器
+      container: container,
+      // 图片列表
+      images: [
+        "./images/banner1.jpg", 
+        "./images/banner2.jpg", 
+        "./images/banner3.jpg"        
+      ],
+
+    });
+
+    slider.on('nav', function( ev ){
+
+      var pageIndex = ev.pageIndex;
+
+      cursors.forEach(function(cursor, index){
+
+        if(index === pageIndex ){
+
+          cursor.className = 'z-active';
+
+        }else{
+
+          cursor.className = '';
+        }
+      })
+
+    })
+})(util);
