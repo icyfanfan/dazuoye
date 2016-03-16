bb6ee1bd3a64cf2b43f970862e5b8dc31108f9a0
hzhuangdi <hzhuangdi@corp.netease.com>
2016-03-16 18:14:16 +0800

    debug
    debug

--- js/index.js
+++ js/index.js
@@ -5,9 +5,9 @@
     var container =  $('.m-sld-wrap')[0];
     var cursors = $('.u-pointer i');
     cursors.forEach(function(cursor, index){
-      cursor.addEventListener('click', function(){
-        slider.nav(index);
-      })
+        cursor.addEventListener('click', function(){
+            slider.nav(index);
+        });
     })
 
     var slider = new Slider({
@@ -19,24 +19,22 @@
         "./images/banner2.jpg", 
         "./images/banner3.jpg"        
       ],
-
     });
 
     slider.on('nav', function( ev ){
-
       var pageIndex = ev.pageIndex;
 
       cursors.forEach(function(cursor, index){
-
         if(index === pageIndex ){
-
           cursor.className = 'z-active';
-
         }else{
-
           cursor.className = '';
         }
-      })
+      });
+    });
 
-    })
+    slider.nav(0);
+    var interval = setInterval(5000,function(){})
+
+
 })(util);
